2f2fcfab7116152376949bf88c5dd00a27576548
ElFami <eldosan0805@gmail.com>
2025-11-26 10:56:10 +0700

    Commit 6 - Read Objects from Files

---
+++ src/file_readingObj.java
@@ -0,0 +1,25 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class file_readingObj {
+    public static void main(String[] args) {
+        Person person = null;
+
+        try(FileInputStream fileIn = new FileInputStream("person.ser");ObjectInputStream in = new ObjectInputStream(fileIn)){
+            person = (Person) in.readObject();
+        }
+        catch (IOException i){
+            i.printStackTrace();
+        }
+        catch (ClassNotFoundException c){
+            System.out.println("Class Not Found");
+            c.printStackTrace();
+        }
+
+        System.out.println("Deserialize person...");
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Campus: " + person.getCampus());
+    }
+}
